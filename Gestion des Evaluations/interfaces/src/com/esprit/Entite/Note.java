1e572a85c2b011ea1f37145c4c99c66f7b157d34
Manel-Fares <manel.fares1@esprit.tn>
2020-02-19 07:58:52 +0100

    Gestion des Evaluations
    interface graphique

---
+++ Gestion des Evaluations/interfaces/src/com/esprit/Entite/Note.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.Entite;
+
+//import java.sql.Date;
+import java.sql.Date;
+
+public class Note {
+
+    private int idEtudiant;
+    private int idMatiere;
+    private int idEnseignant;
+    private Date dateNote;
+    private double noteCC;
+    private double noteDS;
+    private double noteExam;
+    private double moyenne;
+    private String nomMatiere;
+    private float coefficient;
+
+    public Note(String nomMatiere, double moyenne) {
+        this.nomMatiere = nomMatiere;
+        this.moyenne = moyenne;
+    }
+
+    public Note(int idEtudiant, int idMatiere, Date dateNote, double noteCC, double noteDS, double noteExam) {
+        this.idEtudiant = idEtudiant;
+        this.idMatiere = idMatiere;
+        this.dateNote = dateNote;
+        this.noteCC = noteCC;
+        this.noteDS = noteDS;
+        this.noteExam = noteExam;
+    }
+
+    public void setNomMatiere(String nomMatiere) {
+        this.nomMatiere = nomMatiere;
+    }
+
+    public String getNomMatiere() {
+        return nomMatiere;
+    }
+
+    public float getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(int coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public Note(double noteCC, double noteDS, double noteExam, String nomMatiere, float coefficient) {
+        this.noteCC = noteCC;
+        this.noteDS = noteDS;
+        this.noteExam = noteExam;
+        this.nomMatiere = nomMatiere;
+        this.coefficient = coefficient;
+    }
+
+    public Note(int idEtudiant, int idMatiere, Date dateNote, double noteCC, double noteDS, double noteExam, double moyenne) {
+        this.idEtudiant = idEtudiant;
+        this.idMatiere = idMatiere;
+        this.dateNote = dateNote;
+        this.noteCC = noteCC;
+        this.noteDS = noteDS;
+        this.noteExam = noteExam;
+        this.moyenne = moyenne;
+    }
+
+    public Note(int idEtudiant, int idMatiere, Date dateNote, double noteCC, double noteDS, double noteExam, int idEnseignant) {
+        this.idEtudiant = idEtudiant;
+        this.idMatiere = idMatiere;
+        this.idEnseignant = idEnseignant;
+        this.dateNote = dateNote;
+        this.noteCC = noteCC;
+        this.noteDS = noteDS;
+        this.noteExam = noteExam;
+    }
+
+    public int getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public int getIdMatiere() {
+        return idMatiere;
+    }
+
+    public void setIdEnseignant(int idEnseignant) {
+        this.idEnseignant = idEnseignant;
+    }
+
+    public void setCoefficient(float coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public int getIdEnseignant() {
+        return idEnseignant;
+    }
+
+    public Date getDateNote() {
+        return dateNote;
+    }
+
+    public double getNoteCC() {
+        return noteCC;
+    }
+
+    public double getNoteDS() {
+        return noteDS;
+    }
+
+    public double getNoteExam() {
+        return noteExam;
+    }
+
+    public double getMoyenne() {
+        return moyenne;
+    }
+
+    public void setIdEtudiant(int idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public void setIdMatiere(int idMatiere) {
+        this.idMatiere = idMatiere;
+    }
+
+    public void setDateNote(Date dateNote) {
+        this.dateNote = dateNote;
+    }
+
+    public void setNoteCC(double noteCC) {
+        this.noteCC = noteCC;
+    }
+
+    public void setNoteDS(double noteDS) {
+        this.noteDS = noteDS;
+    }
+
+    public void setNoteExam(double noteExam) {
+        this.noteExam = noteExam;
+    }
+
+    public void setMoyenne(double moyenne) {
+        this.moyenne = moyenne;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNote :" + "\n    idEtudiant=" + idEtudiant + ", \n    idMatiere=" + idMatiere
+                + ", \n    dateNote=" + dateNote + ", \n    noteCC=" + noteCC + ", \n    noteDS="
+                + noteDS + ",\n    noteExam=" + noteExam + ", \n    moyenne=" + moyenne + "\n";
+    }
+
+}
---
+++ Gestion des Evaluations/interfaces/src/com/esprit/Service/ServiceNote.java
@@ -0,0 +1,251 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.Service;
+
+import com.esprit.Entite.Matiere;
+import com.esprit.Entite.Note;
+import com.esprit.IService.IService;
+import com.esprit.Utils.DataBase;
+import java.sql.SQLException;
+import java.util.List;
+import java.sql.*;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author House
+ */
+public class ServiceNote implements IService<Note> {
+
+    private Connection con;
+    private Statement ste;
+
+    public ServiceNote() {
+        con = DataBase.getInstance().getConnection();
+    }
+
+    @Override
+    public void ajouter(Note n) throws SQLException {
+        PreparedStatement pre = con.prepareStatement("INSERT INTO  `school`.`Note` ( `idEtudiant`, `idMatiere`,`idEnseignant`, `dateNote`,`noteCC`, `noteDS`, `noteExam`) VALUES ( ?, ?, ? ,?, ?, ?, ?);");
+        pre.setInt(1, n.getIdEtudiant());
+        pre.setInt(2, n.getIdMatiere());
+        pre.setInt(3, n.getIdEnseignant());
+        pre.setDate(4, n.getDateNote());
+        pre.setDouble(5, n.getNoteCC());
+        pre.setDouble(6, n.getNoteDS());
+        pre.setDouble(7, n.getNoteExam());
+        //   pre.setFloat(7, n.getMoyenne());
+        pre.executeUpdate();
+    }
+
+    @Override
+    public void delete(Note n) throws SQLException {
+        String sql = "DELETE FROM Note WHERE idEtudiant=? and idMatiere=? and dateNote=?";
+
+        PreparedStatement pre = con.prepareStatement(sql);
+        pre.setInt(1, n.getIdEtudiant());
+        pre.setInt(2, n.getIdMatiere());
+        pre.setDate(3, n.getDateNote());
+
+        int rowsDeleted = pre.executeUpdate();
+        if (rowsDeleted > 0) {
+            System.out.println("une note a été supprimer avec succès");
+        }
+    }
+
+    @Override
+    public void update(Note n) throws SQLException {
+        String sql = "UPDATE Note SET noteCC=?, noteDS=?, noteExam=? WHERE idEtudiant=? and idMatiere=?";
+
+        PreparedStatement pre = con.prepareStatement(sql);
+        pre.setDouble(1, n.getNoteCC());
+        pre.setDouble(2, n.getNoteDS());
+        pre.setDouble(3, n.getNoteExam());
+        pre.setInt(4, n.getIdEtudiant());
+        pre.setInt(5, n.getIdMatiere());
+       // pre.setDate(6, n.getDateNote());
+
+        int rowsUpdated = pre.executeUpdate();
+        if (rowsUpdated > 0) {
+            System.out.println("une note existante a été mise à jour avec succès!");
+        }
+
+    }
+
+    @Override
+    public List<Note> readAll() throws SQLException {
+        List<Note> arr = new ArrayList<>();
+        ste = con.createStatement();
+        ResultSet rs = ste.executeQuery("select * from Note");
+        while (rs.next()) {
+            int idMatiere = rs.getInt("idMatiere");
+            int idEtudiant = rs.getInt("idEtudiant");
+            Date d = rs.getDate("dateNote");
+            double noteCC = rs.getDouble("noteCC");
+            double noteDS = rs.getDouble("noteDS");
+            double noteExam = rs.getDouble("noteExam");
+            double moyenne = rs.getDouble("moyenne");
+
+            Note n = new Note(idEtudiant, idMatiere, d, noteCC, noteDS, noteExam, moyenne);
+            arr.add(n);
+        }
+        return arr;
+
+    }
+
+    public ObservableList<Note> getNotes() {
+
+        ObservableList<Note> arr = FXCollections.observableArrayList();
+        try {
+            ste = con.createStatement();
+            ResultSet rs = ste.executeQuery("select * from Note ");
+            while (rs.next()) {
+                int idMatiere = rs.getInt("idMatiere");
+                int idEtudiant = rs.getInt("idEtudiant");
+                Date d = rs.getDate("dateNote");
+                double noteCC = rs.getDouble("noteCC");
+                double noteDS = rs.getDouble("noteDS");
+                double noteExam = rs.getDouble("noteExam");
+                double moyenne = rs.getDouble("moyenne");
+
+                Note n = new Note(idEtudiant, idMatiere, d, noteCC, noteDS, noteExam, moyenne);
+                arr.add(n);
+            }
+        } catch (SQLException ex) {
+        }
+        return arr;
+
+    }
+
+    public ObservableList<Note> listeNoteEtudiant(String idE) {
+
+        ObservableList<Note> arr = FXCollections.observableArrayList();
+        try {
+            ste = con.createStatement();
+            ResultSet rs = ste.executeQuery("select n.*,m.nom,m.coef from Note n join matier m "
+                    + "on n.idMatiere=m.id WHERE idEtudiant=" + idE);
+            while (rs.next()) {
+                int idMatiere = rs.getInt("idMatiere");
+                int idEtudiant = rs.getInt("idEtudiant");
+                Date d = rs.getDate("dateNote");
+                double noteCC = rs.getDouble("noteCC");
+                double noteDS = rs.getDouble("noteDS");
+                double noteExam = rs.getDouble("noteExam");
+                double moyenne = rs.getDouble("moyenne");
+                float coefficient = rs.getFloat("coef");
+
+
+                Note n = new Note(idEtudiant, idMatiere, d, noteCC, noteDS, noteExam, moyenne);
+                n.setCoefficient(coefficient);
+                arr.add(n);
+            }
+                    } catch (SQLException ex) {
+        }
+            return arr;
+
+
+    }
+    
+    public ObservableList<Note> listeNoteEtudiantinterface(String idE) {
+
+        ObservableList<Note> arr = FXCollections.observableArrayList();
+        try {
+            ste = con.createStatement();
+            ResultSet rs = ste.executeQuery("select n.*,m.nom,m.coef from Note n join matier m "
+                    + "on n.idMatiere=m.id WHERE idEtudiant=" + idE);
+            while (rs.next()) {
+
+                String nomMatiere = rs.getString("nom");
+                System.out.println(nomMatiere);
+                float coefficient = rs.getFloat("coef");
+                double noteCC = rs.getDouble("noteCC");
+                double noteDS = rs.getDouble("noteDS");
+                double noteExam = rs.getDouble("noteExam");
+                //       double moyenne = rs.getDouble("moyenne");
+            
+                Note n = new Note(noteCC, noteDS, noteExam,nomMatiere, coefficient);
+                arr.add(n);
+            }
+        } catch (SQLException ex) {
+        }
+        return arr;
+
+    }
+        public ObservableList<Note> listeMoyenneNoteEtudiant(String idE) {
+
+        ObservableList<Note> arr = FXCollections.observableArrayList();
+        try {
+            ste = con.createStatement();
+            ResultSet rs = ste.executeQuery("select n.*,m.nom,m.coef from Note n join matier m "
+                    + "on n.idMatiere=m.id WHERE idEtudiant=" + idE);
+            while (rs.next()) {
+
+                String nomMatiere = rs.getString("nom");
+                double moyenne = rs.getDouble("moyenne");
+
+                Note n = new Note(nomMatiere, moyenne);
+                arr.add(n);
+            }
+        } catch (SQLException ex) {
+        }
+        return arr;
+
+    }
+
+    public List<Note> listeNoteEtudiant(int idE) throws SQLException {
+        List<Note> arr = new ArrayList<>();
+        ste = con.createStatement();
+        ResultSet rs = ste.executeQuery("select n.*,m.nom,m.coef from Note n join matier m "
+                    + "on n.idMatiere=m.id WHERE idEtudiant=" + Integer.toString(idE));
+        while (rs.next()) {
+            int idMatiere = rs.getInt("idMatiere");
+            int idEtudiant = rs.getInt("idEtudiant");
+            Date d = rs.getDate("dateNote");
+            double noteCC = rs.getDouble("noteCC");
+            double noteDS = rs.getDouble("noteDS");
+            double noteExam = rs.getDouble("noteExam");
+            double moyenne = rs.getDouble("moyenne");
+
+            Note n = new Note(idEtudiant, idMatiere, d, noteCC, noteDS, noteExam, moyenne);
+            n.setCoefficient(rs.getFloat("coef"));
+            arr.add(n);
+        }
+        return arr;
+
+    }
+
+    public double formuleNote(Note n) {
+
+        return n.getNoteCC() * 0.3 + n.getNoteDS() * 0.2 + n.getNoteExam() * 0.5;
+
+    }
+
+    //permet de calculter moyenne d'une seule matiere
+    public void calculMoyenneNote(Note n) throws SQLException {
+        String sql = "UPDATE Note SET moyenne=? WHERE idEtudiant=? and idMatiere=? and dateNote=?";
+
+        PreparedStatement pre = con.prepareStatement(sql);
+        pre.setDouble(1, formuleNote(n));
+        pre.setInt(2, n.getIdEtudiant());
+        pre.setInt(3, n.getIdMatiere());
+        pre.setDate(4, n.getDateNote());
+
+        int rowsUpdated = pre.executeUpdate();
+        if (rowsUpdated > 0) {
+            System.out.println("moyenne enregistrer avec succes!");
+        }
+
+    }
+
+    public void calculMoyennesNotes(List<Note> l) throws SQLException {
+        for (Note n : l) {
+            calculMoyenneNote(n);
+        }
+
+    }
+}
